2e69fa05d4e48b01ca5da9d6f33d2a1d71692a72
Michael-Edwards777 <Mikee777@rochester.rr.com>
2016-01-15 19:53:09 -0500

    Added framework for drives code

---
+++ src/org/gosparx/team1126/robot/subsystem/Drives.java
@@ -0,0 +1,45 @@
+package org.gosparx.team1126.robot.subsystem;
+
+/**
+ * 
+ * @author Meekly
+ *
+ */
+public class Drives extends GenericSubsystem{
+
+	public Drives(String name, int priority) {
+		super(name, priority);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean init() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void liveWindow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean execute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected long sleepTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void writeLog() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
