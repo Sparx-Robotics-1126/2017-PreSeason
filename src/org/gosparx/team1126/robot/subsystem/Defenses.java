06fbb4c8b75adb47e92dbed34a3dcbd9fd99fd49
Jack Li <jackli8@gmail.com>
2016-01-15 19:55:35 -0500

    Finished framework for Defenses class.

---
+++ src/org/gosparx/team1126/robot/subsystem/Defenses.java
@@ -0,0 +1,42 @@
+package org.gosparx.team1126.robot.subsystem;
+
+/**
+ * Used for breaching the nine defenses.
+ * @author Jack
+ */
+public class Defenses extends GenericSubsystem{
+	
+	public Defenses() {
+		super("Defenses", Thread.NORM_PRIORITY);
+	}
+
+	@Override
+	protected boolean init() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void liveWindow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean execute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected long sleepTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void writeLog() {
+		// TODO Auto-generated method stub
+		
+	}
+}
