aa194364ce7928a64dff069d312f1d53cc6fdd56
allisonmorgan16 <amorgan2559@gmail.com>
2016-01-15 19:54:51 -0500

    I added framework for BallAcq code!

---
+++ src/org/gosparx/team1126/robot/subsystem/BallAcq.java
@@ -0,0 +1,44 @@
+package org.gosparx.team1126.robot.subsystem;
+
+/**
+ * @author Allison
+ */
+
+public class BallAcq extends GenericSubsystem{
+
+	public BallAcq(String name, int priority) {
+		super(name, priority);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean init() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void liveWindow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean execute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected long sleepTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void writeLog() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
