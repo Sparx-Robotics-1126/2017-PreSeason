4857f922ac1077eafce18ea08b40e61b5368ed2f
andrew-thompson <andrewt015@gmail.com>
2016-01-16 12:02:10 -0500

    More framework and comments

--- src/org/gosparx/team1126/robot/subsystem/Scaling.java
+++ src/org/gosparx/team1126/robot/subsystem/Scaling.java
@@ -1,36 +1,72 @@
 package org.gosparx.team1126.robot.subsystem;
 
 import org.gosparx.team1126.robot.IO;
+
 /**
  * Allows the robot to scale the tower
  * @author Andrew Thompson {andrewt015@gmail.com}
  */
 public class Scaling extends GenericSubsystem{
-
-	public Scaling(String name, int priority) {
-		super(name, priority);
+	
+	//******************************OBJECTS***********************************
+	
+	/**
+	 * The only instance of Scaling 
+	 */
+	private static Scaling scaling;
+	
+	/**
+	 * Returns the only instance of scaling
+	 */
+	public static synchronized Scaling getInstance(){
+		if(scaling == null){
+			scaling = new Scaling();
+		}
+		return scaling;
+	}
+	
+	/**
+	 * Creates a new scaling 
+	 */
+	public Scaling() {
+		super("Scaling", Thread.NORM_PRIORITY);
 	}
 
+	/**
+	 * Initializes things 
+	 */
 	@Override
 	protected boolean init() {
 		return false;
 	}
 
+	/**
+	 * Adds items to the live window, overrides genericSubsystems 
+	 */
 	@Override
 	protected void liveWindow() {
 		
 	}
-
+	
+	/**
+	 * Loops
+	 */
 	@Override
 	protected boolean execute() {
 		return false;
 	}
-
+	
+	/**
+	 * Sleep time between loops
+	 */
 	@Override
 	protected long sleepTime() {
 		return 0;
 	}
-
+	
+	/**
+	 * Writes info about the subsystem to the log 
+	 */
 	@Override
 	protected void writeLog() {
 		
