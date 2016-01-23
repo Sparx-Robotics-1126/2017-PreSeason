c36599c7d0d56586e3c5ec4342c595a3f5249b8b
allisonmorgan16 <amorgan2559@gmail.com>
2016-01-23 16:48:20 -0500

    Started State machines, created a bunch of relevant variables and objects, & created the getInstance of method :)

--- src/org/gosparx/team1126/robot/subsystem/BallAcq.java
+++ src/org/gosparx/team1126/robot/subsystem/BallAcq.java
@@ -1,12 +1,105 @@
 package org.gosparx.team1126.robot.subsystem;
 
+import org.gosparx.team1126.robot.sensors.EncoderData;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Encoder;
+
 /**
  * Purpose: to acquire/get the balls and then score them
- * @author Allison
+ * @author Allison and Jack
  */
 
 public class BallAcq extends GenericSubsystem{
 
+//*****************************Constants*******************************************	
+
+	/**
+	 * the distance the blue wheel will travel per tick 
+	 */
+	private static final double DISTANCE_PER_TICK_BLUE = 0;
+	
+	/**
+	 * the distance the arm will travel per tick
+	 */
+	private static final double DISTANCE_PER_TICK_ARM = 0;
+	
+//*****************************Objects*********************************************
+	
+	/**
+	 * creates an instance of BallAcq
+	 */
+	private static BallAcq acq;
+	
+	/**
+	 * the motor that rotates the larger blue wheel
+	 */
+	private CANTalon blueWheelMotor;
+	
+	/**
+	 * the motor that rotates the arm
+	 */
+	private CANTalon armMotor;
+	
+	/**
+	 * the motor that rotates the motor
+	 */
+	private CANTalon rollerMotor;
+	
+	/**
+	 * the encoder that tracks the motion of the blue wheel
+	 */
+	private Encoder blueEncoder;
+	
+	/**
+	 * the encoder that tracks the motion of the arm
+	 */
+	private Encoder armEncoder;
+	
+	/**
+	 * the encoder data for the blue wheel rotation
+	 */
+	private EncoderData blueEncoderData;
+	
+	/**
+	 * the encoder data for the arm rotation
+	 */
+	private EncoderData armEncoderData;
+
+//*****************************Variables*******************************************
+	
+	/**
+	 * the current state of the blue wheel
+	 */
+	private blueWheelState currentBlueState;
+	
+	/**
+	 * the current state of the arm
+	 */
+	private armState currentArmState;
+	
+	/**
+	 * the wanted angle of the blue wheel
+	 */
+	private double wantedBlueAngle;
+	
+	/**
+	 * the wanted angle of the arm
+	 */
+	private double wantedArmAngle;
+	
+//*****************************Methods*********************************************	
+	
+	/**
+	 * makes sure that there is only one instance of BallAcq
+	 * @return a BallAcq object
+	 */
+	public static synchronized BallAcq getInstance(){
+		if(acq == null){
+			acq = new BallAcq();
+		}
+		return acq;
+	}
+	
 	/**
 	 * creates a BallAcq object 
 	 */
@@ -19,7 +112,7 @@ public class BallAcq extends GenericSubsystem{
 	 */
 	@Override
 	protected boolean init() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
@@ -28,7 +121,6 @@ public class BallAcq extends GenericSubsystem{
 	 */
 	@Override
 	protected void liveWindow() {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -38,7 +130,7 @@ public class BallAcq extends GenericSubsystem{
 	 */
 	@Override
 	protected boolean execute() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
@@ -48,8 +140,7 @@ public class BallAcq extends GenericSubsystem{
 	 */
 	@Override
 	protected long sleepTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 20;
 	}
 
 	/**
@@ -57,8 +148,60 @@ public class BallAcq extends GenericSubsystem{
 	 */
 	@Override
 	protected void writeLog() {
-		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/**
+	 * the states for the blue wheel
+	 */
+	public enum blueWheelState{
+		STANDBY,
+		ROTATING,
+		ROTATE_FINDING_HOME;
+		
+		/**
+		 * Gets the state name
+		 * @return the correct state
+		 */
+		@Override
+		public String toString(){
+			switch(this){
+			case STANDBY:
+				return "In standby";
+			case ROTATING:
+				return "Rotating";
+			case ROTATE_FINDING_HOME:
+				return "Rotater Finding Home";
+			default:
+				return "Error :(";
+			}
+		}
+	}
+	
+	/**
+	 * the states for the arm 
+	 */
+	public enum armState{
+		STANDBY,
+		ROTATING,
+		ROTATE_FINDING_HOME;
+	
+		/**
+		 * Gets the state name
+		 * @return the correct state
+		 */
+		@Override
+		public String toString(){
+			switch(this){
+			case STANDBY:
+				return "In Standby";
+			case ROTATING:
+				return "Rotating";
+			case ROTATE_FINDING_HOME:
+				return "Rotate Finding Home";
+			default:
+				return "Error :(";
+			}
+		}
+	}
 }
