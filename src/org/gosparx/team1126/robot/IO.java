213f08483a94caee4dcac8e9a51a832094ba3658
Michael-Edwards777 <Mikee777@rochester.rr.com>
2016-01-30 12:52:08 -0500

    Updated IO

--- src/org/gosparx/team1126/robot/IO.java
+++ src/org/gosparx/team1126/robot/IO.java
@@ -45,6 +45,7 @@ public class IO {
 	public static final int DIO_MAG_RIGHT_HOOK                              = 10;
 	
 
+
 	/**********************************PNU********************************************/
 
 	public static final int PNU_SHIFTER                                     = 0;
