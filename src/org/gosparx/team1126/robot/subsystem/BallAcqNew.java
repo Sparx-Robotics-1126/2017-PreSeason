22efada485b4e46082f86c9a0b86937e162e5f22
Kmodos <amechler1998@gmail.com>
2016-04-09 13:08:49 -0400

    no idea what this import was from

--- src/org/gosparx/team1126/robot/subsystem/BallAcqNew.java
+++ src/org/gosparx/team1126/robot/subsystem/BallAcqNew.java
@@ -4,8 +4,6 @@ import org.gosparx.team1126.robot.IO;
 import org.gosparx.team1126.robot.sensors.EncoderData;
 import org.gosparx.team1126.robot.sensors.MagnetSensor;
 
-import com.sun.scenario.effect.Flood;
-
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
